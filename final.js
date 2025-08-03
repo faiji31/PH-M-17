bbea035039c1728e34b995f0f0bc4c8a553cb0dc
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-04 00:35:30 +0600

    6th commit

--- final.js
+++ final.js
@@ -42,3 +42,14 @@ var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
 //write your code here
 var email = student.name + student.roll +"."+ student.department + "@" + "ph.ac.bd";
 console.log(email);
+
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+var currentsalary =startingSalary;
+for (var i=1; i<=experience;i++){
+    currentsalary*=1.05;
+}
+console.log(currentsalary.toFixed(2));
