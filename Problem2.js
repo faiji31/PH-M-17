63f7ef440cf6fad1c81808aaf74bafcd557b7806
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-08-03 23:51:29 +0600

    2nd commit

---
+++ Problem2.js
@@ -0,0 +1,11 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+if (money>=45000){
+    console.log("Buying Laptop");
+
+}else if(money>=10000){
+    console.log("Buying Cycle");
+}else{
+    console.log("Buying Chocolate");
+}
